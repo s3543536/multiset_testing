20675f8f253418857713a84dd341f9615fb22bcc
1wsx10 <1wsx10@gmail.com>
2017-08-18 14:41:55 +1000

    added some helper functions for BST

--- BstMultiset.java
+++ BstMultiset.java
@@ -6,27 +6,53 @@ import java.lang.Comparable;
 public class BstMultiset<T extends Comparable<T>> extends Multiset<T> {
 
 	public class Node<T> {
-		T item;
-		Node<T> parent;
-		Node<T> leftBranch;
-		Node<T> rightBranch;
+		public T item;
+		public Node<T> parent;
+		public Node<T> left == null;
+		public Node<T> right == null;
 
 		public Node<T>(Node<T> parent, T item) {
 			this.item = item;
 			this.parent = parent;
-			this.leftBranch = null;
-			this.rightBranch = null;
+		}
+
+		public boolean isLeaf() {
+			return left == null && right == null;
+		}
+
+		public Node<T> leftMost() {
+			return (left == null ? right : left)
 		}
 	}
 
+	public Node<T> trunk = null;
+
 	public BstMultiset() {
-		// Implement me!
 	} // end of BstMultiset()
 
-	public void add(T item) {
-		// Implement me!
-	} // end of add()
+	private Node<T> getLeftMostLeaf(Node<T> branch) {
+		Node<T> to_check = branch;
+		
+		if(branch == null) {
+			return null;
+		}
 
+		do {
+			branch = to_check;
+			to_check = branch.leftMost();
+		} while(to_check != null);
+		return branch;
+	}
+
+	public void add(T item) {
+		//TODO Implement me!
+		if(trunk == null) {
+			//empty
+			trunk = new Node<T>(null, item);
+		} else {
+
+		}
+	} // end of add()
 
 	public int search(T item) {
 		// Implement me!
